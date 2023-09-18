6a5d2e8a0da556de487b4edbbdca849c0f7cc96e
lai2-01 <lai2-01@itismeucci.com>
2023-09-18 11:44:36 +0200

    first commit

---
+++ src/main/java/demo/App.java
@@ -0,0 +1,20 @@
+package demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /**
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        String a;
+        a = "ciao";
+        a = null;
+        
+        System.out.println( "Hello World!" );
+    }
+}
